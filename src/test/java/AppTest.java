97aba9f9a45c918adc485d33e3e020eb59bd8d4d
NickOwhadi <50139543+NickOwhadi@users.noreply.github.com>
2019-08-06 20:21:22 -0400

    Update AppTest.java

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,8 +1,7 @@
 public class AppTest {
     public static void main(String[] args) {
-        System.out.println("Hello");
-        System.out.println("Hello");
-        System.out.println("Hello");
+        System.out.println("yo");
+       
 
     }
 }
