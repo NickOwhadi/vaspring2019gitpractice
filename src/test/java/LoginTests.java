dfc20ef003ad7df42a29fe437db41ef40a77a3a8
NickOwhadi <50139543+NickOwhadi@users.noreply.github.com>
2019-08-06 21:38:44 -0400

    Update LoginTests.java

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -2,9 +2,6 @@ public class LoginTests {
     public static void main(String[] args) {
         System.out.println("line 2");
 
-        System.out.println("line 3");
-        System.out.println("line 1");
-
         System.out.println("line 1");
         System.out.println("line 23");
 
