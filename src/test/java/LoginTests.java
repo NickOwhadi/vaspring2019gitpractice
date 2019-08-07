175df7f4cc33ef1e30871b3c97cde0354d25010d
nickOwhadi <n7owhad@hotmail.com>
2019-08-06 21:39:20 -0400

    '1'

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -4,8 +4,6 @@ public class LoginTests {
 
         System.out.println("line 3");
         System.out.println("line 1");
-
-        System.out.println("line 1");
         System.out.println("line 23");
 
     }
